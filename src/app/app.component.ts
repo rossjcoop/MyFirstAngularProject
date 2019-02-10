f4e7714587d99a6cf64a744d5d38162289386c42
Ross Cooper <rossjamescooper@gmail.com>
2019-02-10 14:35:25 -0800

    Added html and style files to component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,19 +4,10 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
-    template: '<h2>Hello World!</h2>',
-    styles: [
-        `
-        h2 {
-            font-family: courier;
-            font-size: 1.2em;
-        }
-        
-        `
-    ]
-
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.css']
 })
 
 export class AppComponent {
-
+    emoji = ['😀', '😝', '😎', '😡'];
 }
