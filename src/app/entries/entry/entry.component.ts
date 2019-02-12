50138c316795c155c4c1d0474fc5407373851c32
Ross Cooper <rossjamescooper@gmail.com>
2019-02-11 23:05:01 -0800

    Added a in-memory web api to simulate a backend server

--- src/app/entries/entry/entry.component.ts
+++ src/app/entries/entry/entry.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Entry } from '../shared/entry.model';
 
 @Component({
     selector: 'app-entry',
@@ -7,12 +8,5 @@ import { Component } from '@angular/core';
 })
 
 export class EntryComponent {
-    title: string = 'My First Photo';
-    photo: string = 'http://placehold.it/800x500?text=Angular Basics';
-    description: string = 'A Description of my First Photo';
-    comments: any[] = [
-        {name: "Ross", comment: "Awesome bro!"},
-        {name: "Ross", comment: "Awesome bro!"},
-        {name: "Ross", comment: "Awesome bro!"},
-    ]
+    @Input() entry: Entry;
 }
